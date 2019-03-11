df52e33b669785994b4a839321523924e4537b5f
Gerardo11011 <gerardo_apg_97@hotmail.com>
2019-03-11 01:52:56 -0600

    problem resolve with a new api

--- index.js
+++ index.js
@@ -20,43 +20,45 @@ function displayResult(data) {
 	items.forEach(function (item) {
 
 
-		let urlVideo = 'https://www.youtube.com/watch?v=${item.id.videoId}';
+		let linkVideo = 'https://www.youtube.com/watch?v=${item.id.videoId}';
     let titleVideo = item.snippet.title;
     let imageVideo = item.snippet.thumbnails.medium.url;
 
 		$('.display').append(`
-							<a href=${urlVideo} target="_blank">
+							<a href=${linkVideo} target="_blank">
 							<h3> ${titleVideo} </h3>
 							<img src= ${imageVideo} alt="videoThumbnail" class="center">
 							</a>
 
 					`);
 	});
-	if(data.hasOwnProperty('prevPageToken')) {
-		$('.display').append('<button id="prev" class="navig"> < </button>');
-
-		$('#prev').on('click', (event) => {
-			let url = 'https://www.googleapis.com/youtube/v3/search?key='+ apiKey + '&part=snippet&type=video&maxResults='+ maxVideos + '&pageToken=' + data.prevPageToken + '&q=' + $('#searchText').val();
-			handleFetch(url, displayResult);
-		});
-
-	}
+	
 	if(data.hasOwnProperty('nextPageToken')) {
-		$('.display').append('<button id="next" class="navig"> > </button>');
+		$('.display').append('<button id="next"> > </button>');
 
 		$('#next').on('click', (event) => {
-			let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&pageToken=' + data.nextPageToken + '&q=' + $('#searchText').val();
-			handleFetch(url, displayResult);
+			let link = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&pageToken=' + data.nextPageToken + '&q=' + $('#searchText').val();
+			handleFetch(link, displayResult);
 		});
 	}
+
+	if(data.hasOwnProperty('prevPageToken')) {
+		$('.display').append('<button id="prev"> < </button>');
+
+		$('#prev').on('click', (event) => {
+			let link = 'https://www.googleapis.com/youtube/v3/search?key='+ apiKey + '&part=snippet&type=video&maxResults='+ maxVideos + '&pageToken=' + data.prevPageToken + '&q=' + $('#searchText').val();
+			handleFetch(link, displayResult);
+		});
+
+	}
+
 }
 
 function watchForm() {
 	$('.youtub').on('submit', (event) => {
 		event.preventDefault();
-
-		let videos = $('#searchText').val();
-		let url = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&part=snippet&type=video&maxResults='+maxVideos+'&q=' + videos;
+		let nombre = $('#searchText').val();
+		let url = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&part=snippet&type=video&maxResults='+maxVideos+'&q=' + nombre;
 		handleFetch(url, displayResult);
 	});
 }
