09f5de0744e68113574dd90f70662132054c4642
Gerardo11011 <gerardo_apg_97@hotmail.com>
2019-03-11 01:24:21 -0600

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let apiKey = "AIzaSyDqFJ123EQFVBlEZazabvRJfTK8rUSxKEU";
-//AIzaSyDqFJ123EQFVBlEZazabvRJfTK8rUSxKEU
+let apiKey = "AIzaSyAR9qtEZGLZCNfmQSWqowE8Mqk1W6y89Ms";
+//AIzaSyB4NRkNulEy17ckwt2klvJAoygFDrh8hIM
 let maxVideos = 10;
+
 function handleFetch(link, callback) {
 	$.ajax({
 		url: link,
@@ -14,47 +15,46 @@ function handleFetch(link, callback) {
 function displayResult(data) {
 	$('.display').html('');
 	var items = data.items;
-	//console.log(data);
 	items.forEach(function (item) {
-    let urlVideo = 'https://www.youtube.com/watch?v=${item.id.videoId}';
+
+
+		let urlVideo = 'https://www.youtube.com/watch?v=${item.id.videoId}';
     let titleVideo = item.snippet.title;
     let imageVideo = item.snippet.thumbnails.medium.url;
 
-
-
 		$('.display').append(`
-							<a href= ${urlVideo} target="_blank">
-							<h3 class="center"> ${titleVideo} </h3>
+							<a href=${urlVideo} target="_blank">
+							<h3> ${titleVideo} </h3>
 							<img src= ${imageVideo} alt="videoThumbnail" class="center">
 							</a>
+
 					`);
 	});
-
-  if(data.hasOwnProperty('nextPageToken')) {
-		$('.display').append('<button id="next" class="display"> > </button>');
-
-		$('#nextBtn').on('click', (event) => {
-			let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&pageToken=' + data.nextPageToken + '&q=' + $('#videoSearchBox').val();
-			handleFetch(url, displayResult);
-		});
-	}
-
-  if(data.hasOwnProperty('prevPageToken')) {
-		$('.display').append('<button id="prev" class="display"> < </button>');
+	if(data.hasOwnProperty('prevPageToken')) {
+		$('.display').append('<button id="previousBtn" class="navig"> < </button>');
 
 		$('#previousBtn').on('click', (event) => {
-			let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&pageToken=' + data.prevPageToken + '&q=' + $('#videoSearchBox').val();
+			let url = 'https://www.googleapis.com/youtube/v3/search?key='+ apiKey + '&part=snippet&type=video&maxResults='+ maxVideos + '&pageToken=' + data.prevPageToken + '&q=' + $('#searchText').val();
 			handleFetch(url, displayResult);
 		});
 
 	}
+	if(data.hasOwnProperty('nextPageToken')) {
+		$('.display').append('<button id="nextBtn" class="navig"> > </button>');
 
+		$('#nextBtn').on('click', (event) => {
+			let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&pageToken=' + data.nextPageToken + '&q=' + $('#searchText').val();
+			handleFetch(url, displayResult);
+		});
+	}
 }
 
 function watchForm() {
 	$('.videoForm').on('submit', (event) => {
 		event.preventDefault();
-		let url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&part=snippet&type=video&maxResults=' + maxVideos + '&q=' + $('#searchText').val();
+
+		let videos = $('#searchText').val();
+		let url = 'https://www.googleapis.com/youtube/v3/search?key='+apiKey+'&part=snippet&type=video&maxResults='+maxVideos+'&q=' + videos;
 		handleFetch(url, displayResult);
 	});
 }
